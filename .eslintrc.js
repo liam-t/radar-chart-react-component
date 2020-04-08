e6f8faada73db32185fb2bfefa154008d3c83741
Liam T <liam.t10@icloud.com>
2020-04-08 12:06:50 +0100

    allowing absolute resolve path from src

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
+const { resolve } = require('path');
+
 module.exports = {
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: [
+          resolve(__dirname, 'node_modules'),
+          resolve(__dirname, 'src'),
+          resolve(__dirname),
+        ],
+      },
+    },
+  },
   extends: [
     'react-app',
     'airbnb',
@@ -10,5 +23,6 @@ module.exports = {
   ],
   rules: {
     'import/extensions': 0,
+    'react/jsx-one-expression-per-line': 0,
   },
 };
